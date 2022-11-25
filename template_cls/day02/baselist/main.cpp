211574b6b5bd3cb841c45bd3ed6a762e28198c13
zhuge20100104 <zhuzhongliang@sensetime.com>
2022-11-26 07:35:23 +0800

    add base list class

--- template_cls/day02/baselist/main.cpp
+++ template_cls/day02/baselist/main.cpp
@@ -40,8 +40,66 @@ public:
 
     // 获取头结点元素
     T& front() {
+        if(empty()) {
+            throw std::underflow_error("null node");
+        }
 
+        return m_head->m_data;
     }
+
+    T const& front() const {
+        return const_cast<list*>(this)->front();
+    }
+
+    // 添加尾节点
+    void push_back(T const& data) {
+        m_tail = new node(m_tail, data, nullptr);
+        if(m_tail->m_prev) {
+            m_tail->m_prev->m_next = m_tail;
+        } else {
+            m_head = m_tail;
+        }
+    }
+
+    // 删除尾节点
+    void pop_back() {
+        if(empty()) {
+            return;
+        }
+
+        node* pnode = m_tail->m_prev;
+        delete m_tail;
+        if(pnode) {
+            pnode->m_next = nullptr;
+        } else {
+            m_head = nullptr;
+        }
+
+        m_tail = pnode;
+    }
+
+    // 获取尾节点数据
+    T& back() {
+        if(empty()) {
+            throw std::underflow_error("null node");
+        }
+        return m_tail->m_data;
+    }
+
+    T const& back() const {
+        return const_cast<list*>(this)->back();
+    }
+
+    // 清空链表
+    void clear() {
+        while(!empty()) {
+            pop_front();
+        }
+    }
+
+
+
+
 
     std::size_t size() {
         std::size_t i = 0;
@@ -63,5 +121,30 @@ private:
     node* m_head;
     node* m_tail;
 
-    friend std::ostream& operator<< (std::ostream& os, list<T> const& l);
+    friend std::ostream& operator<< (std::ostream& os, list<T> const& l) {
+        for(list<T>::node* pnode=l.m_head; pnode; pnode=pnode->m_next) {
+            os << pnode->m_data << ' ';
+        }
+        return os;
+    }
 };
+
+
+int main(int argc, char* argv[]) {
+    list<int> ls;
+
+    for(int i=0; i<5; ++i) {
+        ls.push_front(10 + i);
+    }
+
+    for(int i=0; i<5; ++i) {
+        ls.push_back(100 + i);
+    }
+
+    std::cout << ls << std::endl;
+    ls.pop_front();
+    ls.pop_back();
+    std::cout << ls << std::endl;
+    
+    return EXIT_SUCCESS;
+}
