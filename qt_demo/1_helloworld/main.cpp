95a99689c0f653a7123a726c30fc5250b04e80fd
zhuge20100104 <zhuzhongliang@sensetime.com>
2023-03-10 07:04:47 +0800

    add console app

--- qt_demo/1_helloworld/main.cpp
+++ qt_demo/1_helloworld/main.cpp
@@ -1,1 +1,8 @@
 #include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+
+int main(int argc, char* argv[]) {
+    QCoreApplication app(argc, argv);
+    qDebug() << "Hello World";
+    app.exec();
+}
