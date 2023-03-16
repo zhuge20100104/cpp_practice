bb4534a9ce5304fe31f34cb97064190f3aac997a
zhuge20100104 <zhuzhongliang@sensetime.com>
2023-03-16 09:33:19 +0800

    add resource file demo

---
+++ qt_demo/13_res_file/main.cpp
@@ -0,0 +1,28 @@
+#include <QtCore/QCoreApplication>
+#include <QtCore/QDebug>
+#include <QtCore/QFile>
+#include <string>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <sstream>
+
+void Read(QString const& file_name) {
+    QFile file(file_name);
+    if(!file.open(QFile::ReadOnly | QFile::Text)) {
+        std::cout << "Could not open file: " << file_name.toStdString() << " for read...";
+        return;
+    }
+
+    QTextStream in(&file);
+    auto text = in.readAll();
+    std::cout << text.toStdString();
+    file.close();
+}
+
+int main(int argc, char* argv[]) {
+    QCoreApplication app(argc, argv);
+    QString hello_res = ":/hello_file/conanfile.txt";
+    Read(hello_res);
+    app.exec();
+}
